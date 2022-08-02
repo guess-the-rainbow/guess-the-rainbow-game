338ab80ceca67142e59b033d6a710323fd9f9d04
Brooke Heck <b19heck@gmail.com>
2022-08-02 10:51:07 -0700

    start js skeleton

--- js/app.js
+++ js/app.js
@@ -1,1 +1,43 @@
 'use strict';
+
+// Global Variables
+// User Array from local storage, current user
+
+
+// constructor function, make the USER constructor
+function User(username) {
+  
+}
+
+// prototype functions for user
+User.prototype.updateStats() {
+
+}
+
+// constructor function for GameBoard
+function GameBoard() {
+
+}
+
+// prototype functions for GameBoard
+GameBoard.prototype.addGuess(letterColorArray) {
+
+}
+
+GameBoard.prototype.renderBoard() {
+
+}
+
+GameBoard.prototype.checkGuess() {
+  return [int];
+}
+
+
+GameBoard.prototype.updateBoard() {
+  
+}
+
+
+
+
+// Helper functions
